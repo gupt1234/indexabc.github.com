11d20b65e30bf575e18dd5ef803324de194d185b
gupt1234 <97908251+gupt1234@users.noreply.github.com>
2022-02-20 09:23:36 +0530

    Add files via upload

---
+++ Add.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    if (scrollY >= 180) {
+        navbar.classList.add('bg');
+    } else {
+        navbar.classList.remove('bg');
+    }
+})
